cc1f5511e9dafd511c934192778eb81d9f6022ea
Thennavan Ramalingam <thennavanr@gmail.com>
2012-07-02 11:39:43 -0400

    validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
   attr_accessible :eventdate, :menu, :name, :restaurant
+   validates_presence_of :eventdate, :menu, :name, :restaurant
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   attr_accessible :eventname, :order_item, :username
+   validates_presence_of :eventname, :order_item, :username
   belongs_to :event
   
   def new
